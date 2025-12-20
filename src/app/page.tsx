d793e9d8560df096cf96004ebed479c9d1aa9153
Akshat7garg <akshat7garg@gmail.com>
2025-12-20 23:52:21 +0530

    chore: initialize codebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex w-full p-4 min-h-screen items-center justify-center text-2xl bg-zinc-50 font-sans dark:bg-black">
-      Under Maintainance! Live on or before December 20th, 2025
+      Under Maintainance! Live on or before December 22nd, 2025
     </div>
   );
 }
