91913c19e58633e18a1e4a1cf68112648e210330
Akshat7garg <akshat7garg@gmail.com>
2026-01-08 01:26:35 +0530

    final deployment date

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ### Under Maintenance 🚧
 
-A revamped portfolio launches on or before **January 8, 2026**.
+A revamped portfolio launches on or before **January 11, 2026**.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ function page() {
   return (
     <div className="flex flex-col gap-2 w-full p-4 min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <p className='font-black text-3xl'>Under Maintenance 🚧</p>
-      <p className='text-2xl'>A revamped portfolio launches on or before January 8, 2026.</p>
+      <p className='text-2xl'>A revamped portfolio launches on or before January 11, 2026.</p>
     </div>
   )
 }
