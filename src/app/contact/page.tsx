fd0e7bfa3a2328577db60eda95834af0f8ac4d8b
Akshat7garg <akshat7garg@gmail.com>
2026-01-24 03:13:25 +0530

    Revamp date announcement

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className="flex flex-col gap-2 w-full p-4 min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <p className='font-black text-3xl'>Under Maintenance 🚧</p>
+      <p className='text-2xl'>A revamped portfolio launches on or before February 1, 2026.</p>
+    </div>
+  )
+}
+
+export default page
