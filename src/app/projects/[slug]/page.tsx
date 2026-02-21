6a18357e4653129c4aad2745754488235e9d8907
Akshat7garg <akshat7garg@gmail.com>
2026-02-21 21:12:01 +0530

    feat(routing): implement routing flow

---
+++ src/app/projects/[slug]/page.tsx
@@ -0,0 +1,8 @@
+export default function ProjectDetailsPage() {
+    return (
+        <div className="flex flex-col gap-2 w-full p-4 min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <p className='font-black text-3xl'>Under Maintenance 🚧</p>
+            <p className='text-2xl'>A revamped portfolio launches on or before February 25, 2026.</p>
+        </div>
+    );
+}
